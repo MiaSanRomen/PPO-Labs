16acc7fa370bd8f71090a51fd2dc33bc5b49d72c
Roman Brizinski <62285014+MiaSanRomen@users.noreply.github.com>
2021-01-08 10:58:54 +0300

    Update Converter.java

--- Lab1/app/src/main/java/com/example/myapplication/Converter.java
+++ Lab1/app/src/main/java/com/example/myapplication/Converter.java
@@ -9,4 +9,28 @@ public class Converter {
         }
         return "";
     }
+    static public String convertTemperature(String data, AdditionalUnitEnum input, AdditionalUnitEnum output) {
+        if (input.getUnit() == output.getUnit()) {
+            if (!data.equals("")) {
+                if(input == AdditionalUnitEnum.CELSIUS){
+                    return String.valueOf(fromCelsius(Double.parseDouble(data), output));
+                }
+                else if (output == AdditionalUnitEnum.CELSIUS){
+                    return String.valueOf(toCelsius(Double.parseDouble(data), input));
+                }
+                return String.valueOf(fromCelsius(toCelsius(Double.parseDouble(data), input), output));
+            }
+        }
+        return "";
+    }
+
+    static public double toCelsius(double data, AdditionalUnitEnum unit)
+    {
+        return (data - unit.getAddCoefficient()) / unit.getCoefficient();
+    }
+
+    static public double fromCelsius(double data, AdditionalUnitEnum unit)
+    {
+        return data * unit.getCoefficient() + unit.getAddCoefficient();
+    }
 }
