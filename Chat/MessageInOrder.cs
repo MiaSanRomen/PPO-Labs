ac63b0fbc9abfd800a93a15792f7c367735deaee
Roman Brizinski <62285014+MiaSanRomen@users.noreply.github.com>
2021-03-18 00:39:19 +0300

    Create MessageInOrder.cs

---
+++ Chat/MessageInOrder.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace chat
+{
+    public class MessageInOrder
+    {
+        public int Nomber;
+        public string Message;
+        public MessageInOrder(int nomber, string message)
+        {
+            Nomber = nomber;
+            Message = message;
+        }
+    }
+}
